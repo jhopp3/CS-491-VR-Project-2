a6c87789f426193bc27ff0247a47cca65a915e7c
Jon Hopp <jhopp3@uic.edu>
2017-10-10 03:37:50 -0500

    Initial load json function.

--- Andy Solar System Test/Assets/Planets.cs
+++ Andy Solar System Test/Assets/Planets.cs
@@ -6,6 +6,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 
 public class Planets : MonoBehaviour {
@@ -21,6 +22,8 @@ public class Planets : MonoBehaviour {
 
 	float panelXScale = 2.0F;
 	float orbitXScale = 2.0F;
+
+	private string JSONFile = "data.json";
 
 
 	//------------------------------------------------------------------------------------//
@@ -302,8 +305,36 @@ public class Planets : MonoBehaviour {
 
 	//------------------------------------------------------------------------------------//
 
+	private void LoadPlanetData()
+	{
+		Debug.Log("LoadPlanetData");
+		// Path.Combine combines strings into a file path
+		// Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
+		string filePath = Path.Combine(Application.streamingAssetsPath, JSONFile);
+
+		if(File.Exists(filePath))
+		{
+
+			Debug.Log("File.Exists");
+			// Read the json from the file into a string
+			string dataAsJson = File.ReadAllText(filePath); 
+			// Pass the json to JsonUtility, and tell it to create a GameData object from it
+//			GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+
+			// Retrieve the allRoundData property of loadedData
+//			allRoundData = loadedData.allRoundData;
+		}
+		else
+		{
+			Debug.LogError("Cannot load game data!");
+		}
+	}
+
+	//------------------------------------------------------------------------------------//
+
 	void Start () {
 		//radius (km), name, type, spectral classification, luminosity
+
 		string[] sol = new string[5] { "695500", "Our Sun", "sol", "G2V" , "1.0"};
 
 		//orbit radius (km), radius (km), orbit period (yr), texture, name
