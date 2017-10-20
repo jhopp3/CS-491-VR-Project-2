5c38f9b6a76f1a847a86b43d380514c0a7aebf55
Jon Hopp <jhopp3@uic.edu>
2017-10-20 03:33:16 -0500

    Added star textures.  Marked special cases.

--- Andy Solar System Test/Assets/Star.cs
+++ Andy Solar System Test/Assets/Star.cs
@@ -11,6 +11,8 @@ public class Star {
 	public double radius;
 //	public string spectralClassification; // Same as type?
 	public int numberOfPlanets;
+	public string texture;
+	public char spectralType;
 
 	public Star (PlanetData pd) {
 		luminosity = pd.st_lum;
@@ -19,6 +21,66 @@ public class Star {
 		type = pd.st_spstr;
 		radius = pd.st_rad;
 		numberOfPlanets = pd.pl_pnum;
+
+		setTexture();
+	}
+
+	private void setTexture() {
+		if (!string.IsNullOrEmpty(type)) {
+			// Debug.LogError("Type is " + type);
+			spectralType = type[0];
+			spectralType = Char.ToLower(spectralType);
+
+			switch (spectralType)
+			{
+				case 'a':
+					texture = "astar";
+					break;
+				case 'b':
+					texture = "bstar";
+					break;
+				case 'f':
+					texture = "fstar";
+					break;
+				case 'g':
+					texture = "gstar";
+					break;
+				case 'k':
+					texture = "kstar";
+					break;
+				case 'm':
+					texture = "mstar";
+					break;
+				case 'l':
+					// Binary system of two brown dwarfs
+					// https://en.wikipedia.org/wiki/DENIS-P_J082303.1-491201_b
+					texture = "mstar";
+					break;
+				case 't':
+					// Binary system of two brown dwarfs
+					// https://en.wikipedia.org/wiki/WISE_1217%2B1626
+					texture = "mstar";
+					break;
+				case 'o':
+					texture = "ostar";
+					break;
+				case 'w':
+					// WD https://en.wikipedia.org/wiki/NN_Serpentis
+					// Handle 2 stars here.
+					texture = "bstar";
+					break;
+				case 's':
+					// sdBV https://en.wikipedia.org/wiki/Subdwarf_B_star
+					texture = "bstar";
+					break;
+				default:
+					Debug.LogError("Invalid Spectral Type: " + spectralType);
+					break;
+			}
+
+		} else {
+			// Debug.LogError("Spectral Type is null");
+		}
 	}
 
 	public override string ToString()
